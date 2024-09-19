9b1e72a8ad7911fd9b3412b1ed22c71bba6585f6
Adrian Lam <adrianlamdev@gmail.com>
2024-09-18 19:40:38 -0700

    feat: scrap subjects

--- src/scraper/scraper.py
+++ src/scraper/scraper.py
@@ -1,11 +1,22 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 
-class Scraper:
+class CourseScraper:
     def __init__(self, config, url):
         self.config = config
-        self.url = url
+        self.subjects = self.get_subjects()
+
+    def extract_subject(self, subject):
+        pass
+
+    def get_subjects(self):
+        # https://vancouver.calendar.ubc.ca/course-descriptions/courses-subject
+        url = "https://vancouver.calendar.ubc.ca/course-descriptions/courses-subject"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        raw_subjects = soup.find_all("ol", class_="list-buttons")
 
     def scrape(self):
         pass
@@ -20,3 +31,7 @@ class Scraper:
         data = self.scrape()
         processed_data = self.process_data(data)
         self.save_to_supabase(processed_data)
+
+
+test = CourseScraper("config", "url")
+test.get_subjects()
